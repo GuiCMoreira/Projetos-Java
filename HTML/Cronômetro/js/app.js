16d66028a7e0e5a9253211e8c45837ce96fe07b6
Guilherme Carvalho <gui0307carvalho@gmail.com>
2023-09-25 08:30:38 -0300

    Update

--- HTML/Cronômetro/js/app.js
+++ HTML/Cronômetro/js/app.js
@@ -1,34 +1,93 @@
-let hor = document.getElementById("horas")
-let min = document.getElementById("minutos")
-let seg = document.getElementById("segundos")
+// Variáveis para controle do cronômetro
+let startTime
+let interval
+let running = false
+const maxMarks = 5
+const markTimes = []
 
-let startbotao = document.getElementById("bstart")
-let markbotao = document.getElementById("bmark")
-let stopbotao = document.getElementById("bstop")
-let resetbotao = document.getElementById("breset")
-let times = document.getElementById("times")
+// Elementos HTML
+const tempoElement = document.getElementById("tempo")
+const bstartElement = document.getElementById("bstart")
+const bmarkElement = document.getElementById("bmark")
+const bstopElement = document.getElementById("bstop")
+const bresetElement = document.getElementById("breset")
+const marcacoesElement = document.querySelector(".marcacoes")
 
-let parado = true
-
-startbotao.addEventListener("click", startcronometro)
-markbotao.addEventListener("click", markcronometro)
-stopbotao.addEventListener("click", stopcronometro)
-resetbotao.addEventListener("click", resetcronometro)
-
-function startcronometro() {
-  cronometro = setInterval(function () {
-    seg++
-    if (seg === 60) {
-      seg = 0
-      min++
-    }
-    min.textContent = min < 10 ? "0" + min : min
-    seg.textContent = seg < 10 ? "0" + seg : seg
-  }, 1000)
+// Função para formatar o tempo no cronômetro
+function formatTime(ms) {
+  const minutes = Math.floor(ms / 60000)
+  const seconds = Math.floor((ms % 60000) / 1000)
+  const milliseconds = ms % 1000
+  return `${minutes.toString().padStart(2, "0")}:${seconds
+    .toString()
+    .padStart(2, "0")}:${milliseconds.toString().padStart(3, "0")}`
 }
 
-function markcronometro() {}
+// Função para adicionar uma marcação
+function addMark() {
+  const currentTime = tempoElement.textContent
+  markTimes.unshift(currentTime) // Adicionar no início do array
 
-function stopcronometro() {}
+  // Remover a marcação mais antiga se houver mais do que o máximo
+  if (markTimes.length > maxMarks) {
+    markTimes.pop() // Remover do final do array
+  }
 
-function resetcronometro() {}
+  // Atualizar a lista de marcações na interface
+  updateMarkList()
+}
+
+// Função para atualizar a lista de marcações na interface
+function updateMarkList() {
+  marcacoesElement.innerHTML = ""
+  for (let i = 0; i < markTimes.length; i++) {
+    const markElement = document.createElement("span")
+    markElement.textContent = markTimes[i]
+    marcacoesElement.appendChild(markElement)
+  }
+}
+
+// Função para atualizar o cronômetro
+function updateTimer() {
+  const currentTime = Date.now()
+  const elapsedTime = currentTime - startTime
+  tempoElement.textContent = formatTime(elapsedTime)
+}
+
+// Evento de clique no botão "START"
+bstartElement.addEventListener("click", () => {
+  if (!running) {
+    startTime = Date.now() - (markTimes.length > 0 ? parseInt(markTimes[0]) : 0)
+    interval = setInterval(updateTimer, 10)
+    running = true
+    bstartElement.value = "PAUSE"
+  } else {
+    clearInterval(interval)
+    running = false
+    bstartElement.value = "RESUME"
+  }
+})
+
+// Evento de clique no botão "MARK"
+bmarkElement.addEventListener("click", () => {
+  if (running) {
+    addMark()
+  }
+})
+
+// Evento de clique no botão "STOP"
+bstopElement.addEventListener("click", () => {
+  clearInterval(interval)
+  running = false
+  bstartElement.value = "START"
+})
+
+// Evento de clique no botão "RESET"
+bresetElement.addEventListener("click", () => {
+  clearInterval(interval)
+  running = false
+  bstartElement.value = "START"
+  tempoElement.textContent = "00:00:000"
+  markTimes.length = 0 // Limpa todas as marcações
+  marcacoesElement.innerHTML = ""
+})
