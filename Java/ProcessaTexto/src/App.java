006ab6ef6aefb020ae6c869a048b6e710f601541
Guilherme Carvalho <gui0307carvalho@gmail.com>
2023-10-16 09:00:40 -0300

    update

--- Java/ProcessaTexto/src/App.java
+++ Java/ProcessaTexto/src/App.java
@@ -6,8 +6,10 @@ import java.util.List;
 
 public class App {
     public static void main(String[] args) throws Exception {
-
         Path caminhoEntrada = Paths.get("src/processatexto/entrada.txt");
+        List<String> dados;
+        Path localGravacao = Paths.get("src/processatexto/saida.txt");
+
 
         List<String> dados = lerArquivo(caminhoEntrada);
         try {
@@ -17,15 +19,26 @@ public class App {
             System.exit(0);
         }
 
-        novosDados = processarArquivo(dados);
+        String novosDados = processarArquivo(dados);
         gravarArquivo(localGravacao, novosDados);
+    }
+
+    private static void gravarArquivo(Path local, String info){
 
     }
 
     static List<String> lerArquivo(Path arquivo) throws IOException {
-
         return Files.readAllLines(arquivo);
+    }
 
+    private static String processarArquivo(List<String> info) {
+        String invertido = "";
+
+        for (String linha : info) {
+            invertido = linha + invertido;
+        }
+        
+        return invertido;
     }
 
 }
