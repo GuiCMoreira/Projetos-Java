637271435faa8cab0d2d15f288dee6c339a48b69
Guilherme Carvalho <gui0307carvalho@gmail.com>
2023-10-25 15:40:05 -0300

    update

--- Java/Atividade Teams 01111/src/App.java
+++ Java/Atividade Teams 01111/src/App.java
@@ -1,3 +1,4 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class App {
@@ -8,13 +9,18 @@ public class App {
         boolean sair = false;
         while (sair == false) {
 
-            System.out.println("Qual conversão você deseja realizar?");
-            System.out.println("1 - Centímetros para polegadas");
-            System.out.println("2 - Polegadas para centímetros");
-            System.out.println("3 - Graus Celsius para graus Fahrenheit");
-            System.out.println("4 - Graus Fahrenheit para graus Celsius");
-            System.out.println("5 - Sair");
-            int operacao = tcl.nextInt();
+            System.out.println("-----------------------------------------------------");
+            System.out.println("| Menu                                              |");
+            System.out.println("-----------------------------------------------------");
+            System.out.println("| 1 - Centímetros para polegadas                    |");
+            System.out.println("| 2 - Polegadas para centímetros                    |");
+            System.out.println("| 3 - Graus Celsius para graus Fahrenheit           |");
+            System.out.println("| 4 - Graus Fahrenheit para graus Celsius           |");
+            System.out.println("| 5 - Sair                                          |");
+            System.out.println("-----------------------------------------------------");
+            System.out.println("Escolha uma operação: ");
+
+            int operacao = VerificarInteiro();
 
             switch (operacao) {
                 case 1:
@@ -30,28 +36,55 @@ public class App {
                     FahCel();
                     break;
                 case 5:
+                    System.out.println("Saindo...");
                     sair = true;
+                    break;
+                default:
+                    System.out.println("Operação inválida, insira uma das opções do menu: ");
                     break;
             }
         }
     }
 
-    static void VerificarOperacao(String operacao) {
-        try {
-            int op = Integer.parseInt(operacao);
-            if (op < 1 || op > 5) {
-                throw new Exception("Operação inválida");
+    // Funções de verificação ---------------------------------------------
+
+    static int VerificarInteiro() {
+        int valor = 0;
+        boolean entradaValida = false;
+        do {
+            try {
+                valor = tcl.nextInt();
+                entradaValida = true;
+            } catch (InputMismatchException e) {
+                System.out.println("Operação inválida. Insira um número inteiro.");
+                tcl.nextLine();
             }
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
+        } while (entradaValida == false);
+
+        return valor;
+    }
+
+    static double VerificarDouble() {
+        double valor = 0;
+        boolean entradaValida = false;
+        do {
+            try {
+                valor = tcl.nextDouble();
+                entradaValida = true;
+            } catch (InputMismatchException e) {
+                System.out.println("Operação inválida. Insira um número.");
+                tcl.nextLine();
+            }
+        } while (entradaValida == false);
+
+        return valor;
     }
 
     // Funções de conversão ---------------------------------------------
 
     static double CentPol() {
         System.out.println("Insira o valor em centímetros: ");
-        double cent = tcl.nextDouble();
+        double cent = VerificarDouble();
         double pol = cent / 2.54f;
         System.out.println("O valor em polegadas é: " + pol);
         return pol;
@@ -59,7 +92,7 @@ public class App {
 
     static double PolCent() {
         System.out.println("Insira o valor em polegadas: ");
-        double pol = tcl.nextDouble();
+        double pol = VerificarDouble();
         double cent = pol * 2.54f;
         System.out.println("O valor em centímetros é: " + cent);
         return cent;
@@ -67,7 +100,7 @@ public class App {
 
     static double CelFah() {
         System.out.println("Insira o valor em graus Celsius: ");
-        double cel = tcl.nextDouble();
+        double cel = VerificarDouble();
         double fah = (cel * 9 / 5) + 32;
         System.out.println("O valor em graus Fahrenheit é: " + fah);
         return fah;
@@ -75,7 +108,7 @@ public class App {
 
     static double FahCel() {
         System.out.println("Insira o valor em graus Fahrenheit: ");
-        double fah = tcl.nextDouble();
+        double fah = VerificarDouble();
         double cel = (fah - 32) * 5 / 9;
         System.out.println("O valor em graus Celsius é: " + cel);
         return cel;
