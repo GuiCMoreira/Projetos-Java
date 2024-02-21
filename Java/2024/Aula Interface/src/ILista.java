930f12f53bcdd0bc9df623e4dc8af37b8cb47ad2
Guilherme Carvalho <gui0307carvalho@gmail.com>
2024-02-21 13:10:23 -0300

    update

---
+++ Java/2024/Aula Interface/src/ILista.java
@@ -0,0 +1,9 @@
+public interface ILista {
+
+  public void inserir(String dado);
+
+  public String excluir(String dado);
+
+  public boolean consulta(String dado);
+
+}
---
+++ Java/2024/Aula Interface/src/ListaRegras.java
@@ -0,0 +1,21 @@
+public class ListaRegras implements ILista {
+
+  @Override
+  public void inserir(String dado) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'inserir'");
+  }
+
+  @Override
+  public String excluir(String dado) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'excluir'");
+  }
+
+  @Override
+  public boolean consulta(String dado) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'consulta'");
+  }
+  
+}
