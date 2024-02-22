64c7ea52b4b4d55448bd84f772c198d419484167
Guilherme Carvalho <gui0307carvalho@gmail.com>
2024-02-22 14:06:31 -0300

    update

--- HTML/Aula1011/js/index.js
+++
@@ -1,9 +0,0 @@
-let arquivo = document.getElementById("arquivo")
-arquivo.addEventListener("change", function () {
-  lerArquivo("arquivo", "info")
-})
-
-let executar = document.getElementById("exibir")
-executar.addEventListener("click", function () {
-  exibirGrafico("info", "grafico")
-})
