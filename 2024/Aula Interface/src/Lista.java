fd95191e4721242831e385b6f8d34c0f01857e35
Guilherme Carvalho <gui0307carvalho@gmail.com>
2024-02-26 13:07:40 -0300

    update

--- 2024/Aula Interface/src/Lista.java
+++ 2024/Aula Interface/src/Lista.java
@@ -1,11 +1,26 @@
+import java.util.Scanner;
+
 public class Lista {
     static String[] ranking = new String[10];
+    static Scanner tcl = new Scanner(System.in);
 
     public static void main(String[] args) throws Exception {
-        
-        ListaRegras.inserir("Abacaxi", ranking);
-        ListaRegras.excluir("Abacaxi", ranking);
-        ListaRegras.consulta("Abacaxi", ranking);
+
+        for (String valor : ranking) {
+            System.out.println(valor);
+        }
+
+        ListaRegras.inserir("2000", ranking);
+
+        for (String valor : ranking) {
+            System.out.println(valor);
+        }
+
+        ListaRegras.inserir("1000", ranking);
+
+        for (String valor : ranking) {
+            System.out.println(valor);
+        }
 
     }
 }
--- 2024/Aula Interface/src/ListaRegras.java
+++ 2024/Aula Interface/src/ListaRegras.java
@@ -1,17 +1,48 @@
 public class ListaRegras {
 
   public static void inserir(String dado, String vetor[]) {
-    System.out.println("Inseri.");
+
+    if (vetor[vetor.length - 1] == null ||
+        Integer.parseInt(vetor[vetor.length - 1]) < Integer.parseInt(dado)) {
+      vetor[vetor.length - 1] = dado;
+    }
+
+    reordenar(vetor);
+
+    System.out.println("Inseri");
+
   }
 
   public static String excluir(String dado, String vetor[]) {
+
     System.out.println("Exclui.");
     return null;
+
   }
 
   public static boolean consulta(String dado, String vetor[]) {
+
     System.out.println("Consultei.");
     return false;
+
   }
-  
+
+  private static void reordenar(String vetor2[]) {
+
+    for (byte pos = (byte) vetor2.length; pos > 0; pos--) {
+
+      if (vetor2[pos - 1] == null || Integer.parseInt(vetor2[pos]) > Integer.parseInt(vetor2[pos - 1])) {
+
+        String aux = vetor2[pos - 1];
+        vetor2[pos - 1] = vetor2[pos];
+        vetor2[pos] = aux;
+
+      } else {
+        break;
+      }
+
+    }
+
+  }
+
 }
