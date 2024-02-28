ad1ddded0f675a1205e6df31edf539d7b6c0b087
Guilherme Carvalho <gui0307carvalho@gmail.com>
2024-02-28 12:26:41 -0300

    update

---
+++ 2024/Lista Matriz/src/Lista.java
@@ -0,0 +1,23 @@
+public class Lista {
+  
+  public static boolean estaCheia(String[][] matriz) {
+    return true;
+  }
+
+  public static boolean estaVazia(String[][] matriz) {
+    return true;
+  }
+
+  public static void inserir(String nome, int quantidade, String[][] matriz) {
+
+  }
+
+  public static String[][] chamar(int lugares, String[][] matriz) {
+    return null;
+  }
+
+  public static String exibir(String[][] matriz) {
+    return "";
+  }
+
+}
