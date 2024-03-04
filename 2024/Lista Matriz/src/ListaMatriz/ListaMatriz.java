a735245aba12b5b0c558ae898842afb6833b8b26
Guilherme Carvalho <gui0307carvalho@gmail.com>
2024-03-04 12:38:59 -0300

    update

--- 2024/Lista Matriz/src/ListaMatriz/ListaMatriz.java
+++ 2024/Lista Matriz/src/ListaMatriz/ListaMatriz.java
@@ -5,9 +5,10 @@ public class ListaMatriz {
 
         String[][] espera = new String[10][2];
 
-        System.out.println("Lista vazia: ");
+        System.out.println("\nImprimindo a lista vazia: \n");
         System.out.println(Lista.exibir(espera));
 
+        System.out.println("Lista depois de inserir as pessoas: \n");
         Lista.inserir("Gui", 2, espera);
         Lista.inserir("Mafe", 4, espera);
         Lista.inserir("Jão", 1, espera);
@@ -15,19 +16,15 @@ public class ListaMatriz {
         Lista.inserir("Paloma", 3, espera);
         Lista.inserir("Nicolas", 2, espera);
         Lista.inserir("Gabriel", 9, espera);
-
-        System.out.println("Lista depois de inserir as pessoas: ");
         System.out.println(Lista.exibir(espera));
 
+        System.out.println("Chamando pessoa que reservou 4 lugares: (Mafe)\n");
         var chamando = Lista.chamar(4, espera);
-
-        System.out.println("Chamando pessoa que reservou 4 lugares: ");
         System.out.println("Chamando " + chamando[0]);
-
         System.out.println(Lista.exibir(espera));
 
-        System.out.println("Inserindo uma nova pessoa no lugar da pessoa que chamei anteriormente: ");
-        Lista.inserir("Gab", 7, espera);
+        System.out.println("Inserindo uma nova pessoa no lugar da pessoa que chamei anteriormente: (Rick, 7 lugares)\n");
+        Lista.inserir("Rick", 7, espera);
         System.out.println(Lista.exibir(espera));
 
 
