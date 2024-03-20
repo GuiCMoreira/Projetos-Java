02da7575c73ee258d2c5f22ef89a02b74e2c2c54
Guilherme Carvalho <gui0307carvalho@gmail.com>
2024-03-20 14:07:42 -0300

    update

--- 2024/TorreDeHanoiPilha/src/pilhavetor/App.java
+++ 2024/TorreDeHanoiPilha/src/pilhavetor/App.java
@@ -127,7 +127,7 @@ public class App {
         }
     }
 
-    static boolean verificarTorreVazia(int torre) {
+    static boolean verificarDiscos(int origem, int destino) {
 
         return true;
     }
