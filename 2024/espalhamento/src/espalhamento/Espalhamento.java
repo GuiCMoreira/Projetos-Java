ba1505d9c6b9af07db8ea50b449d21b781de360e
Guilherme Carvalho <gui0307carvalho@gmail.com>
2024-04-08 08:50:55 -0300

    update

--- 2024/espalhamento/src/espalhamento/Espalhamento.java
+++ 2024/espalhamento/src/espalhamento/Espalhamento.java
@@ -1,18 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package espalhamento;
 
-/**
- *
- * @author Luciano Pelissoli
- */
 public class Espalhamento {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         int qtd = 200;
         Hash.criar(qtd);
