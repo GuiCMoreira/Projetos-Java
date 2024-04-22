dbb937f061ff6121f628d03b9690c45a3d01f1c6
Guilherme Carvalho <gui0307carvalho@gmail.com>
2024-04-22 13:02:13 -0300

    update

--- 2024/Lista Encadeada/src/listaencadeada/ListaEncadeada.java
+++ 2024/Lista Encadeada/src/listaencadeada/ListaEncadeada.java
@@ -6,8 +6,11 @@ public class ListaEncadeada {
     private static Lista<Paciente> prioritario;
 
     public static void main(String[] args) throws Exception {
+        normal = new Lista<Paciente>();
+        prioritario = new Lista<Paciente>();
         normal.insert(new Paciente("João", 20));
         normal.insert(new Paciente("Gui", 29));
         prioritario.insert(new Paciente("Maria", 30));
+        System.exit(0);
     }
 }
