0e31791ac9401e6a9fd96d2f0b0f330cdb56c7e3
Guilherme Carvalho <gui0307carvalho@gmail.com>
2024-08-20 13:58:25 -0300

    Refactor pessoa class to include nascimento property

--- 2024/2 Semestre/projeto1/src/main/java/br/fatec/projeto1/modelo/pessoa.java
+++ 2024/2 Semestre/projeto1/src/main/java/br/fatec/projeto1/modelo/pessoa.java
@@ -17,4 +17,34 @@ public class pessoa {
     this.cpf = cpf;
   }
 
+  public pessoa(String nome, String cpf, String nascimento) {
+    this.nome = nome;
+    this.cpf = cpf;
+    this.nascimento = nascimento;
+  }
+
+  public String getNome() {
+    return nome;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+  public String getCpf() {
+    return cpf;
+  }
+
+  public void setCpf(String cpf) {
+    this.cpf = cpf;
+  }
+
+  public String getNascimento() {
+    return nascimento;
+  }
+
+  public void setNascimento(String nascimento) {
+    this.nascimento = nascimento;
+  }
+
 }
